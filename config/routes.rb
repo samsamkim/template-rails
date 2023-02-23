051af87f59450eeee0144d05203d73c2f7fe8ce7
Michael Wang <wang.michael213@gmail.com>
2023-02-23 11:12:46 -0600

    welcome page without ActiveRecord

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,15 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  Rails.application.routes.draw do
+    root "articles#index"
+  
+    get "/articles", to: "articles#index"
+  end
+  
+  Rails.application.routes.draw do
+    get "/articles", to: "articles#index"
+  
+    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  end
 end
