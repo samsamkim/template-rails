d02c24e308df2d411b3b9d6e59d9d035ecb44302
Michael Wang <wang.michael213@gmail.com>
2023-02-28 15:07:34 -0600

    custom error page

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,12 @@
 Rails.application.routes.draw do
+  get 'errors/internal_server_error'
+  root "articles#index"
+  get "/articles", to: "articles#index"
+  match "/404", to: "errors#not_found", via: :all
+  match "/500", to: "errors#internal_server_error", via: :all
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  Rails.application.routes.draw do
-    root "articles#index"
-  
-    get "/articles", to: "articles#index"
-  end
-  
-  Rails.application.routes.draw do
-    get "/articles", to: "articles#index"
-  
     # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  end
 end
