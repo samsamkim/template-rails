64816dde2efce10dea59915c2722ae385923245b
Michael Wang <wang.michael213@gmail.com>
2023-02-28 17:05:39 -0600

    update custom error page text

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
-  get 'errors/internal_server_error'
-  root "articles#index"
-  get "/articles", to: "articles#index"
-  match "/404", to: "errors#not_found", via: :all
-  match "/500", to: "errors#internal_server_error", via: :all
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  get 'errors/internal_server_error'
   # Defines the root path route ("/")
   # root "articles#index"
-    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root "articles#index"
+  get "/articles", to: "articles#index"
+  
+  # routing 404 and 500 errors to our custom error page.
+  match "/404", to: "errors#not_found", via: :all
+  match "/500", to: "errors#internal_server_error", via: :all
+
 end
